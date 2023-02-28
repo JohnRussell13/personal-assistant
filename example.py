726802fb398d2e4dd420a954c641d5c04a735596
JohnRussell13 <aleksandar.g1999@gmail.com>
2023-02-28 09:12:15 +0100

    Comments

--- example.py
+++ example.py
@@ -4,18 +4,23 @@ import os
 from dotenv import load_dotenv
 import yaml
 
+# hidden and public config
 load_dotenv()
 with open('config.yaml') as f:
     config = yaml.load(f, Loader=yaml.FullLoader)
 
+# extracting config
 GOOGLE_SEARCH_API_KEY = os.environ.get('GOOGLE_SEARCH_API_KEY')
+CX = config['GOOGLE_SEARCH']['CX']
+
+# query string
 query = "Greece"
-cx = config['GOOGLE_SEARCH']['CX']
 
-url = f"https://www.googleapis.com/customsearch/v1?q={query}&key={GOOGLE_SEARCH_API_KEY}&cx={cx}"
-
+# send request
+url = f"https://www.googleapis.com/customsearch/v1?q={query}&key={GOOGLE_SEARCH_API_KEY}&cx={CX}"
 response = requests.get(url)
 
+# parse request
 if response.status_code == 200:
     results = json.dumps(response.json(), indent=2)
 
